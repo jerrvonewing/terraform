88935be36c21bfca0f4e7b4aae0d8df72b664221
jerrvon <jerrvonewing.cs@gmail.com>
2023-12-12 07:47:55 -0600

    Created a config that would create an ec2 instance and attach an elastic ip address

---
+++ eip/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0230bd60aa48260c6"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "elasticip" {
+  instance = aws_instance.ec2.id
+}
+
+output "EIP" {
+  value = aws_eip.elasticip.public_ip
+}
